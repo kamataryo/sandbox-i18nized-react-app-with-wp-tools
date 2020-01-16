32cad4dabef4d8ff41f0efdeea86e2dda0268fd5
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2020-01-16 17:16:30 +0900

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { __ } from "@wordpress/i18n";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{__("Hello, WordPress tools!")}</p>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { setLocaleData } from "@wordpress/i18n";
+import jaData from "./lang/ja.json";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const match = window.location.search.match(/^\?lang=(.*)$/);
+const lang = match ? match[1] : "en";
+
+if (lang === "ja") {
+  console.log(jaData);
+  setLocaleData(jaData.locale_data.messages);
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
